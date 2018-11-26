50f9ddbf2f1d5867bbb481105a551e21c3950b81
fizoratti <robson.felipe@acad.pucrs.br>
2018-11-26 08:50:07 -0200

    Uncomment some code

--- src/App.java
+++ src/App.java
@@ -4,23 +4,17 @@ import java.util.List;
 
 public class App {
     public static void main(String args[]) {
-        /*
-         * Message message = new Message("Hello World!");
-         * System.out.println(message.getMessage());
-         * 
-         * Scanner ler = new Scanner(System.in);
-         * 
-         * String texto = ler.next();
-         * 
-         * ler.close();
-         * 
-         * System.out.println(texto);
-         */
 
-        List<Pessoa> pessoas = new ArrayList<>();
-        pessoas.add(new Pessoa("Felipe", 23, Pessoa.Genero.MASC));
+        Message message = new Message("Hello World!");
+        System.out.println(message.getMessage());
 
-        Pessoa.printPessoas(pessoas, p -> p.getIdade() >= 18 && p.getGenero() == Pessoa.Genero.MASC);
+        Scanner ler = new Scanner(System.in);
+
+        String texto = ler.next();
+
+        ler.close();
+
+        System.out.println(texto);
 
     }
 }
