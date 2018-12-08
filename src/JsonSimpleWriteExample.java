1e5822765c802b1b5a11717f7346f470ce8260eb
fizoratti <robson.felipe@acad.pucrs.br>
2018-12-08 21:10:34 -0200

    Include code to read json file

--- src/JsonSimpleWriteExample.java
+++ src/JsonSimpleWriteExample.java
@@ -1,11 +1,17 @@
 import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
 
 public class JsonSimpleWriteExample {
 
@@ -32,7 +38,7 @@ public class JsonSimpleWriteExample {
             for (Pessoa p : lista) {
                 pessoas.add(p.toJSON());
             }
-            tabela.put("Pessoas:", pessoas);
+            tabela.put("Pessoas", pessoas);
             file.write(tabela.toJSONString());
             file.flush();
 
@@ -40,16 +46,68 @@ public class JsonSimpleWriteExample {
             System.err.format("Erro de E/S: %s%n", e);
         }
 
-        // -------------------------------
+        // JSONParser parser = new JSONParser();
+        // Reader reader = new FileReader("Pessoas.json");
 
-        Path path1 = Paths.get("teste2.csv");
-        try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(path1, Charset.defaultCharset()))) {
-            for (Pessoa p : lista) {
-                writer.println(p.getNome() + ";" + p.getIdade() + ";" + p.getGenero());
+        // Object jsonObj = parser.parse(reader);
+
+        // JSONObject jsonObject = (JSONObject) jsonObj;
+
+        // String name = (String) jsonObject.get("name");
+        // System.out.println("Name = " + name);
+
+        // long age = (Long) jsonObject.get("age");
+        // System.out.println("Age = " + age);
+
+        // JSONArray cities = (JSONArray) jsonObject.get("cities");
+
+        // @SuppressWarnings("unchecked")
+        // Iterator<String> it = cities.iterator();
+        // while (it.hasNext()) {
+        // System.out.println("City = " + it.next());
+        // }
+        // reader.close();
+
+        Path path1 = Paths.get("Pessoas.json");
+        try (BufferedReader reader = Files.newBufferedReader(path1, Charset.defaultCharset())) {
+
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(reader);
+            JSONObject tabela = (JSONObject) obj;
+
+            JSONArray pessoas = (JSONArray) tabela.get("Pessoas");
+            System.out.println(pessoas.toJSONString());
+            if (pessoas == null)
+                System.out.println("É Null");
+
+            System.out.println("Tamanho de Pessoas: " + pessoas.size());
+
+            @SuppressWarnings("unchecked")
+            Iterator<JSONObject> it = pessoas.iterator();
+            while (it.hasNext()) {
+                System.out.println("City = " + it.next());
             }
+            reader.close();
+
+        } catch (ParseException e) {
+            System.err.format("Erro de E/S: %s%n", e);
         } catch (IOException e) {
             System.err.format("Erro de E/S: %s%n", e);
+        } catch (Exception e) {
+            System.err.format("Erro de E/S: %s%n", e);
         }
+
+        // -------------------------------
+
+        // Path path1 = Paths.get("teste2.csv");
+        // try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(path1,
+        // Charset.defaultCharset()))) {
+        // for (Pessoa p : lista) {
+        // writer.println(p.getNome() + ";" + p.getIdade() + ";" + p.getGenero());
+        // }
+        // } catch (IOException e) {
+        // System.err.format("Erro de E/S: %s%n", e);
+        // }
 
     }
 
