0894210a6573209f2c450dc60b39366055799fa8
fizoratti <robson.felipe@acad.pucrs.br>
2018-12-08 19:04:28 -0200

    Write a json file with an array of pessoas from an ArrayList<Pessoa>

--- src/JsonSimpleWriteExample.java
+++ src/JsonSimpleWriteExample.java
@@ -11,33 +11,36 @@ public class JsonSimpleWriteExample {
 
     public static void main(String[] args) {
 
-        JSONObject obj = new JSONObject();
-        obj.put("name", "mkyong.com");
-        obj.put("age", new Integer(100));
+        ArrayList<Pessoa> lista = new ArrayList<>();
+        lista.add(new Pessoa("Jose", 12, Pessoa.Genero.MASC));
+        lista.add(new Pessoa("Maria", 14, Pessoa.Genero.FEM));
+        lista.add(new Pessoa("Jesus", 1, Pessoa.Genero.MASC));
+        lista.add(new Pessoa("Judas", 2, Pessoa.Genero.MASC));
+        lista.add(new Pessoa("Madalena", 2, Pessoa.Genero.FEM));
+        lista.add(new Pessoa("Moisés", 6, Pessoa.Genero.MASC));
 
-        JSONArray list = new JSONArray();
-        list.add("msg 1");
-        list.add("msg 2");
-        list.add("msg 3");
+        // JSONArray list = new JSONArray();
+        // list.add("msg 1");
+        // list.add("msg 2");
+        // list.add("msg 3");
 
-        obj.put("messages", list);
+        // obj.put("messages", list);
 
-        try (FileWriter file = new FileWriter("test.json")) {
-
-            file.write(obj.toJSONString());
+        try (FileWriter file = new FileWriter("Pessoas.json")) {
+            JSONObject tabela = new JSONObject();
+            JSONArray pessoas = new JSONArray();
+            for (Pessoa p : lista) {
+                pessoas.add(p.toJSON());
+            }
+            tabela.put("Pessoas:", pessoas);
+            file.write(tabela.toJSONString());
             file.flush();
 
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.format("Erro de E/S: %s%n", e);
         }
 
-        System.out.print(obj);
-
         // -------------------------------
-
-        ArrayList<Pessoa> lista = new ArrayList<>();
-        lista.add(new Pessoa("Jose", 12, Pessoa.Genero.MASC));
-        // ...inclui mais pessoas na lista
 
         Path path1 = Paths.get("teste2.csv");
         try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(path1, Charset.defaultCharset()))) {
