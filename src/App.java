e3a29eb41b25d9951ba681239d0719eba6aac37a
fizoratti <robson.felipe@acad.pucrs.br>
2018-12-07 23:15:43 -0200

    Remove old commented code

--- src/App.java
+++ src/App.java
@@ -4,18 +4,6 @@ import java.util.List;
 
 public class App {
     public static void main(String args[]) {
-        /*
-         * Message message = new Message("Hello World!");
-         * System.out.println(message.getMessage());
-         * 
-         * Scanner ler = new Scanner(System.in);
-         * 
-         * String texto = ler.next();
-         * 
-         * ler.close();
-         * 
-         * System.out.println(texto);
-         */
 
         List<Pessoa> pessoas = new ArrayList<>();
         pessoas.add(new Pessoa("Felipe", 23, Pessoa.Genero.MASC));
